f09a37f98ae64034173771aad523f5e4201edcee
Frank Yu <frankyu1999@gmail.com>
2022-03-04 04:08:11 -0500

    Hardcode host name for REST requests

--- src/main/resources/static/spa.js
+++ src/main/resources/static/spa.js
@@ -1,5 +1,5 @@
 let addressBooks = [];
-const host = location.hostname
+const host = 'whispering-lowlands-82355.herokuapp.com'
 
 // Render according to locally stored addressBooks var
 function renderAddressBooks() {
@@ -49,7 +49,7 @@ function renderAddressBook(elem) {
 function deleteBuddy(bookId, buddyId) {
     $.ajax({
         type: "DELETE",
-        url: `http://${host}/api/book/${bookId}/buddy/${buddyId}`
+        url: `https://${host}/api/book/${bookId}/buddy/${buddyId}`
     }).then(data => {
         // update data + rerender
         getAddressBooks();
@@ -59,7 +59,7 @@ function deleteBuddy(bookId, buddyId) {
 function createBuddy(bookId) {
     $.ajax({
         type: "POST",
-        url: `http://${host}/api/book/${bookId}/buddy`,
+        url: `https://${host}/api/book/${bookId}/buddy`,
         data: JSON.stringify({
             name: $('#name').val(),
             address: $('#address').val(),
@@ -79,7 +79,7 @@ function createBuddy(bookId) {
 
 function getAddressBooks() {
     $.ajax({
-        url: "http://${host}/api/book/"
+        url: "https://${host}/api/book/"
     }).then(data => {
         addressBooks = data;
         renderAddressBooks();
@@ -89,7 +89,7 @@ function getAddressBooks() {
 function createAddressBook() {
     $.ajax({
         type: "POST",
-        url: "http://${host}/api/book/"
+        url: "https://${host}/api/book/"
     }).then(data => {
         addressBooks.push(data);
         // Update UI
